9ebc00921f2f58cfaa45d9537266b114f3808604
Daniela Morariu <dany.morariu@gmail.com>
2023-04-03 17:32:21 +0300

    Implement show functions

---
+++ index.js
@@ -0,0 +1,32 @@
+var activePage = 'home';
+
+var homePage = document.getElementById(activePage);
+homePage.style.display = 'block'
+
+function showSkills () {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('languages').style.display = 'none';
+    document.getElementById('projects').style.display = 'none';
+    document.getElementById('skills').style.display = 'block'
+}
+
+function showProjects () {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('languages').style.display = 'none';
+    document.getElementById('skills').style.display = 'none';
+    document.getElementById('projects').style.display = 'block'
+}
+
+function showLanguages () {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('skills').style.display = 'none';
+    document.getElementById('projects').style.display = 'none';
+    document.getElementById('languages').style.display = 'block';
+}
+
+function showHome () {
+    document.getElementById('languages').style.display = 'none';
+    document.getElementById('skills').style.display = 'none';
+    document.getElementById('projects').style.display = 'none';
+    document.getElementById('home').style.display = 'block';
+}
