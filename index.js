09e366ae735cb20ef7d3598741227c23218e5294
Daniela Morariu <daniela.morariu@target-e.com>
2023-04-05 17:03:46 +0300

    Prettier formatted

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-var activePage = 'skills';
+var activePage = "skills";
 
 show(activePage);
 
 function hide(id) {
-    document.getElementById(id).style.display = 'none';
+  document.getElementById(id).style.display = "none";
 }
 
 function show(id) {
-    document.getElementById(id).style.display = 'block';
+  document.getElementById(id).style.display = "block";
 }
 
 function hideAllPages() {
-    var pages = document.querySelectorAll('#main .page');
-    pages.forEach(function (page) {
-        hide(page.id);
-    });
+  var pages = document.querySelectorAll("#main .page");
+  pages.forEach(function (page) {
+    hide(page.id);
+  });
 }
 
 function showPage(id) {
-    hideAllPages();
-    show(id);
+  hideAllPages();
+  show(id);
 }
 
-document.querySelector("#top-menu-bar").addEventListener('click', function (e) {
-    if (e.target.matches("a") || e.target.closest("a")) {
-        var id = e.target.dataset.page;
-        showPage(id);
-    }
+document.querySelector("#top-menu-bar").addEventListener("click", function (e) {
+  if (e.target.matches("a") || e.target.closest("a")) {
+    var id = e.target.dataset.page;
+    showPage(id);
+  }
 });
 
 function showSkills() {
-    var skills = [
-        {
-            name: "HTML",
-            endorcements: 5,
-        },
-        {
-            name: "CSS",
-            endorcements: 10,
-        },
-        {
-            name: "JS",
-            endorcements: 15,
-        },
-    ];
-    var html = skills.map(function (skill) {
-         return `<li>${skill.name} - <span class="endorcements">${skill.endorcements}<span></li>`;
-    });
-    var container = document.querySelector("#skills ul");
-    container.innerHTML = html.join("");
+  var skills = [
+    {
+      name: "HTML",
+      endorcements: 5,
+    },
+    {
+      name: "CSS",
+      endorcements: 10,
+    },
+    {
+      name: "JS",
+      endorcements: 15,
+    },
+  ];
+  var html = skills.map(function (skill) {
+    return `<li>${skill.name} - <span class="endorcements">${skill.endorcements}<span></li>`;
+  });
+  var container = document.querySelector("#skills ul");
+  container.innerHTML = html.join("");
 }
 
 showSkills();
