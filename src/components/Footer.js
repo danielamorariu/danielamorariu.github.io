035d4bea6d0659eaa01a6ae1d04cd9c9e2f34d54
Daniela Morariu <daniela.morariu@target-e.com>
2024-05-12 21:00:22 +0300

    Footer section: styling and templating

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,15 @@
+
 export const Footer = () => {
 
     return (
-        <div className="footer">
+        <div className="footer" id="footer">
             <div className="footer-wrapper inner">
-                <h2>Footer</h2>
-                <p>Hendrerit pellentesque tincidunt augue malesuada elit cubilia nisi senectus
-                    molestie dapibus fermentum gravida vel phasellus sagittis finibus consequat cursus mi
-                    feugiat eros id elementum fringilla sit sodales consectetur velit eget orci nostra
-                    luctus class cras nisl ut maecenas aliquet quis ridiculus commodo massa blandit facilisi
-                    egestas vulputate quisque eleifend pede curae porttitor et</p>
+                <div className="footer__socials">
+                    <a href="https://linkedin.com" target="_blank" title="LinkedIn"><span className="footer__social-item footer__social-item--linkedin"></span></a>
+                    <a href="https://facebook.com" target="_blank" title="Facebook"><span className="footer__social-item footer__social-item--facebook"></span></a>
+                    <a href="https://instagram.com" target="_blank" title="Instagram"><span className="footer__social-item footer__social-item--instagram"></span></a>
+                </div>
+                <p>Copyright 2024</p>
             </div>
         </div>
     )
