6b8164ee7939ce88f7998959b691fc09090da2c0
Daniela Morariu <dany.morariu@gmail.com>
2024-08-19 21:57:07 +0300

    [WIP] Projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,6 +10,17 @@ export const Projects = () => {
                         <span></span>
                     </div>
                 </div>
+                <div className="projects__list">
+                    <div className="projects__item projects__item--shop">
+                        <div className="projects__item-content">
+                            <h3>Natural Drop</h3>
+                            <p>Project description: Mattis est vehicula eu eleifend blandit torquent porttitor pretium fusce sollicitudin justo et aliquet sagittis inceptos sed</p>
+                            <p>React + Bootstrap</p>
+                            <p>E-commerce style</p>
+                            <button className="projects__item-button">To project</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
